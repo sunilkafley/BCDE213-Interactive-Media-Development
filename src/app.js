04de6d358fc21a82b2e2a49f30976b355fd8f276
sunilkafley <suk0424@arastudent.ac.nz>
2026-05-18 19:44:16 +1200

    Initial Leaflet project setup improvisation

--- src/app.js
+++ src/app.js
@@ -1,34 +1,5 @@
-// Create map
-const map = L.map('map').setView([-43.5321, 172.6362], 13);
+import { initMap } from './map/initMap.js'
 
-// Add OpenStreetMap tiles
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution:
-    '&copy; OpenStreetMap contributors'
-}).addTo(map);
+const map = initMap()
 
-// Load GeoJSON file
-fetch('data/trees.geojson')
-  .then(response => response.json())
-  .then(data => {
-
-    // Add GeoJSON layer
-    L.geoJSON(data, {
-
-      // Popup for each feature
-      onEachFeature: function (feature, layer) {
-
-        const popupContent = `
-          <strong>${feature.properties.name}</strong><br>
-          Fruit Type: ${feature.properties.fruit}
-        `;
-
-        layer.bindPopup(popupContent);
-      }
-
-    }).addTo(map);
-
-  })
-  .catch(error => {
-    console.error('Error loading GeoJSON:', error);
-  });
+console.log('Fruit Finder Started')
---
+++ src/map/initMap.js
@@ -0,0 +1,16 @@
+export function initMap() {
+
+  const map = L.map('map').setView(
+    [-43.5321, 172.6362],
+    12
+  )
+
+  L.tileLayer(
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    {
+      attribution: '&copy; OpenStreetMap contributors'
+    }
+  ).addTo(map)
+
+  return map
+}
